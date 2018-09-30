eb7016e755069695d883974d36cf0ee6a9f0d8ce
Eduardo <eoschonhofen@inf.ufrgs.br>
2018-09-30 10:48:29 -0300

    Criado algumas queries de sql

---
+++ Models/Database/INSERT_INTO_USUARIO.sql
@@ -0,0 +1,72 @@
+INSERT INTO Usuario VALUES
+(
+  NULL,
+  "Eduardo Osielski Schonhofen",
+  20,
+  "M",
+  "batataFrita123",
+  5.6,
+  NULL,
+  327,
+  false,
+  true,
+  true
+);
+
+INSERT INTO Usuario VALUES
+(
+  NULL,
+  "Felipe Bastos",
+  22,
+  "M",
+  "ffbastos",
+  7.2,
+  NULL,
+  524,
+  false,
+  true,
+  false
+);
+
+INSERT INTO Usuario VALUES
+(
+  NULL,
+  "Matheus Kovaleski",
+  23,
+  "M",
+  "nazihitler",
+  2.3,
+  NULL,
+  666,
+  true,
+  false,
+  false
+);
+INSERT INTO Usuario VALUES
+(
+  NULL,
+  "Giovane",
+  24,
+  "M",
+  "racket",
+  7.2,
+  NULL,
+  150,
+  true,
+  false,
+  false
+);
+INSERT INTO Usuario VALUES
+(
+  NULL,
+  "Cassiano",
+  24,
+  "M",
+  "csGO",
+  0,
+  NULL,
+  0,
+  false,
+  false,
+  true
+);
--- Models/Database/database.js
+++ Models/Database/database.js
@@ -19,3 +19,5 @@ buscaPacientes()
     }
   )
 };
+
+system.log(buscaPacientes);
