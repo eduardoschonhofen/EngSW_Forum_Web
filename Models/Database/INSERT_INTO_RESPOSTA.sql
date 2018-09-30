ebdd80f82e3e8c1579a32c7f0086e023827a1670
Eduardo <eoschonhofen@inf.ufrgs.br>
2018-09-30 12:04:01 -0300

    Criado inserção de RESPOSTA

---
+++ Models/Database/INSERT_INTO_RESPOSTA.sql
@@ -0,0 +1,42 @@
+INSERT INTO Resposta(nomeUsuario,perguntaTitulo,texto,data,mediaAvaliacao,totalDeAvaliacoes) VALUES
+(
+  "eoschonhofen",
+  "Como realizar uma cirurgia no olho sem enxergar?",
+  "Boa tarde Kovaleski! Para realizar a cirurgia no olho,sem enxerga,basta que você utilize vá ao meu consultório,pague 500 reais, que eu faço a cirurgia para você sem enxergar. Obrigado!",
+  now(),
+  10.0,
+  50
+);
+
+INSERT INTO Resposta(nomeUsuario,perguntaTitulo,texto,data,mediaAvaliacao,totalDeAvaliacoes) VALUES
+
+(
+  "ffbastos",
+  "Como realizar uma cirurgia no olho sem enxergar?",
+  "Fala meu consagrado!Para realizar a cirurgia,utilize uma faca de cortar carne,e um maçarico,como é no olho você não precisa enxergar,somente sentir,use o maçarico para cauterizar depois.Obrigado!Não se esqueça de avaliar minha resposta!",
+  now(),
+  5.9,
+  500
+);
+
+INSERT INTO Resposta(nomeUsuario,perguntaTitulo,texto,data,mediaAvaliacao,totalDeAvaliacoes) VALUES
+
+(
+  "eoschonhofen",
+  "Como realizar uma cirurgia no olho sem enxergar?",
+  "A resposta do Felipe está errada,o único jeito certo é você ir ao meu consultória e pagar 500 reais! ",
+  now(),
+  2.0,
+  350
+);
+
+INSERT INTO Resposta(nomeUsuario,perguntaTitulo,texto,data,mediaAvaliacao,totalDeAvaliacoes) VALUES
+
+(
+  "ffbastos",
+  "Como costurar meu braço de volta?",
+  "Não utilize linhas de costura, utilize super bonder para colar o braço junto com os nervos,boa tarde!",
+  now(),
+  10.0,
+  2500
+);
