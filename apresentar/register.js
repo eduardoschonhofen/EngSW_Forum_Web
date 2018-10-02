d6dd592bdc1adb1d4034b51dab02a3b74db83f2c
ufrarion <cassianodab@gmail.com>
2018-10-01 22:05:33 -0300

    Inicio register

---
+++ apresentar/register.js
@@ -0,0 +1,23 @@
+function getNome()
+{
+	return document.getElementById("name").value;
+}
+
+function getNomeUsuario()
+{
+	return document.getElementById("username").value;
+}
+
+function getSenhaUsuario()
+{
+	return document.getElementById("password").value;
+}
+
+document.getElementById('submit').addEventListener("click", function() {
+	var xhr = new XMLHttpRequest();
+	var url = "register";
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-Type", "application/json");
+	var data = JSON.stringify({"name": getNome(), "username": getNomeUsuario(), "password": getSenhaUsuario()});
+	xhr.send(data);
+})
