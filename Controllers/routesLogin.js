1375a5f88bc244e105fe45e230e1100ab63ae102
Eduardo <eoschonhofen@inf.ufrgs.br>
2018-10-02 01:53:34 -0300

    Menos bugs

--- Controllers/routesLogin.js
+++ Controllers/routesLogin.js
@@ -6,22 +6,43 @@ var fs = require('fs');
 
 function realizarLogin(req,res,con)
 {
-  dbUsuario.loginUsuario(con,"gafonseca","racket").then(function(results)
-  {
-    if(results[0]==undefined)
-  {
-    res.writeHead(200, {'Content-Type': 'application/json'});
-    res.write(JSON.stringify("false"));
-    return res.end();
+  var body = '';
+  req.on('data', function (data) {
+      body += data;
 
-  }
-    else
-    {
-      res.writeHead(200, {'Content-Type': 'application/json'});
-      res.write(JSON.stringify("true"));
-      return res.end();
-    }
+      // 1e6 === 1 * Math.pow(10, 6) === 1 * 1000000 ~~~ 1MB
+      if (body.length > 1e6) {
+          // FLOOD ATTACK OR FAULTY CLIENT, NUKE REQUEST
+          req.connection.destroy();
+      }
   });
+  req.on('end', function () {
+
+
+      resultados=JSON.parse(body);
+      console.log(resultados);
+      dbUsuario.loginUsuario(con,resultados.name,resultados.password).then(function(results)
+      {
+        if(results[0]==undefined)
+      {
+        res.writeHead(200, {'Content-Type': 'application/json'});
+        res.write(JSON.stringify("false"));
+        return res.end();
+
+      }
+        else
+        {
+          res.writeHead(200, {'Content-Type': 'application/json'});
+          res.write(JSON.stringify("true"));
+          return res.end();
+        }
+      });
+
+
+  });
+
+
+
 
 }
 
