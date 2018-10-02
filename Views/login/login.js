2134a64386dde6b611b90e280db53a7b5bb3f583
ufrarion <cassianodab@gmail.com>
2018-10-02 01:47:40 -0300

    Login modificado

--- Views/login/login.js
+++ Views/login/login.js
@@ -1,26 +1,49 @@
+saveCookie = function(name, value) {
+	return document.cookie = `${name}=${value}` + "; expires=Fri, 31 Dec 9999 23:59:59 GMT" + "; path=/";
+};
+
+getCookie = function(name) {
+	var parts, value;
+	value = "; " + document.cookie;
+	parts = value.split("; " + name + "=");
+	if (parts.length === 2) {
+		return parts.pop().split(";").shift();
+	} else {
+		return "";
+	}
+};
 
 function getNomeUsuario()
 {
-	return document.getElementsByName('first')[0].value;
+	return document.getElementById("username").value;
 }
 
 function getSenhaUsuario()
 {
-	return document.getElementsByName('last')[0].value;
+	return document.getElementById("password").value;
 }
 
 document.getElementById('submit').addEventListener("click", function() {
 	var xhr = new XMLHttpRequest();
-	var url = "checkLogin";
+	var url = "login";
 	xhr.open("POST", url, true);
 	xhr.setRequestHeader("Content-Type", "application/json");
+
 	xhr.onreadystatechange = function () {
 		if (xhr.readyState === 4 && xhr.status === 200) {
-			var json = JSON.parse(xhr.responseText);
+			var success = xhr.responseText;
 			console.log(xhr.responseText);
+			
+			if (success == "true")
+			{
+				alert("Login EXISTE");
+			}
+			else {
+				alert("Login NÃO EXISTE");
+			}
 		}
 	};
-	var data = JSON.stringify({"nomeUsuario": getNomeUsuario(), "senha": getSenhaUsuario()});
-	xhr.send(data);
 
+	var data = JSON.stringify({"username": getNomeUsuario(), "password": getSenhaUsuario()});
+	xhr.send(data);
 })
