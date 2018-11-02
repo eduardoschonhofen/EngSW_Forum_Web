1972dca59a730d38bc38a65cb3c54d62cc7e9ae1
Eduardo <eoschonhofen@inf.ufrgs.br>
2018-11-02 15:48:49 -0300

    Correção banco de dados na nuvem

--- Controllers/routes.js
+++ Controllers/routes.js
@@ -5,6 +5,7 @@ var dbModerador = require('../Models/Database/dbModerador.js');
 var dbUsuario = require('../Models/Database/dbUsuario.js');
 var dbPergunta = require('../Models/Database/dbPergunta.js');
 var dbResposta = require('../Models/Database/dbResposta.js');
+var dbCreate = require('../Models/Database/dbCreate.js');
 var routesTeste = require('../Controllers/routesTeste.js');
 var routesLogin = require('../Controllers/routesLogin.js');
 var routesRegister = require('../Controllers/routesRegister.js');
@@ -14,7 +15,10 @@ class Routes
 {
   get(req,res,path)
   {
+
     var indexOfDot = path.indexOf('.');
+    if(indexOfDot!=0)
+    {
     var subPath =path.substr(1, indexOfDot-1); // parse da string (remove '/' inicial e após o '.')
     var type = path.substr(indexOfDot+1); // html, css, js, ...
     var folder='./Views'
@@ -29,6 +33,13 @@ class Routes
         res.write(data);
         return res.end();
       });
+    }
+    else
+    {
+      if(path=="/create")
+        dbCreate.create(con);
+
+    }
     }
 
   post(req,res,con)
---
+++ Models/Database/dbCreate.js
@@ -0,0 +1,12 @@
+function create(con)
+{
+  query="CREATE TABLE IF NOT EXISTS Usuario(nomeUsuario VARCHAR(255) NOT NULL,PRIMARY KEY (nomeUsuario),nome varchar(255) NOT NULL,idade INT,sexo VARCHAR(2),senha VARCHAR(255),especialidade VARCHAR(255),mediaAvaliacao FLOAT,somaDeAvaliacoes FLOAT,totalDeAvaliacoes INT,ePaciente BOOLEAN,eMedico BOOLEAN,eModerador BOOLEAN);CREATE TABLE IF NOT EXISTS Pergunta(titulo varchar(255),PRIMARY KEY(titulo),nomeUsuario VARCHAR(255) NOT NULL,FOREIGN KEY(nomeUsuario) REFERENCES Usuario(nomeUsuario),texto TEXT NOT NULL,data DATE,mediaAvaliacao FLOAT,somaDeAvaliacoes FLOAT,totalDeAvaliacoes INT);CREATE TABLE IF NOT EXISTS Resposta(resposta_id INTEGER AUTO_INCREMENT,PRIMARY KEY(resposta_id),nomeUsuario VARCHAR(255) NOT NULL,FOREIGN KEY(nomeUsuario) REFERENCES Usuario(nomeUsuario),perguntaTitulo VARCHAR(255) NOT NULL,FOREIGN KEY(perguntaTitulo) REFERENCES Pergunta(titulo),texto TEXT NOT NULL,data DATE,mediaAvaliacao FLOAT,somaDeAvaliacoes FLOAT,totalDeAvaliacoes INT);";
+  con.query(query,function(error,results){
+    if(error)
+    {
+      return console.error(error.message);
+    }
+})
+}
+
+exports.create=create;
