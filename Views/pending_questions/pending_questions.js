7f7e5f2378ffc2a5ae4f3f0954bb7faa6157cc37
giovaneaf <giovaneaf95@hotmail.com>
2018-11-06 23:11:10 -0200

    fix

--- Views/pending_questions/pending_questions.js
+++ Views/pending_questions/pending_questions.js
@@ -1,7 +1,7 @@
 
 (function() {
   this.printTopic = function(title, msg, username) {
-    return document.getElementById("listTopic").innerHTML += ` <div class="w3-card-4 topic">\n		 <header class="w3-container w3-teal">\n		   <h3 class="title">${title}</h3>\n		 </header>\n\n		 <div class="w3-container">\n			 <p class="username"><b>Usuário: ${username}</b></p>\n		   <p class="msg">${msg}</p>\n		 </div>\n</div> <div class="w3-card-4 topic">		 <header class="w3-container w3-teal">		   <h3 class="title">title</h3>		 </header>		 <div class="w3-container">			 <p class="username"><b>Usuário: eu</b></p>		   <p class="msg">oi</p>		 </div>  </div> <p><button class="w3-btn w3-teal" onclick='ans()'>Aprovar pergunta</button></p>`;
+    return document.getElementById("listTopic").innerHTML += ` <div class="w3-card-4 topic">\n		 <header class="w3-container w3-teal">\n		   <h3 class="title">${title}</h3>\n		 </header>\n\n		 <div class="w3-container">\n			 <p class="username"><b>Usuário: ${username}</b></p>\n		   <p class="msg">${msg}</p>\n		 </div>\n</div> <p><button class="w3-btn w3-teal" onclick='ans()'>Aprovar pergunta</button></p>`;
   };
 
 }).call(this);
