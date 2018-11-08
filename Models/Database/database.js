499315068458ee92a8def9429c5563e2331b0e82
Eduardo <eoschonhofen@inf.ufrgs.br>
2018-11-07 23:33:06 -0200

    BUGS

--- Models/Database/database.js
+++ Models/Database/database.js
@@ -1,14 +1,21 @@
 mysql=require('mysql');
-
-var database = mysql.createPool({
+var database;
+function connect()
+{
+if(!database)
+{
+database = mysql.createPool({
   connectionLimit : 10,
   host: "us-cdbr-iron-east-01.cleardb.net",
   user: "b089b876f46b39",
   password: "e7e02c90",
   database: "heroku_cb42695d67403c5"
 });
+}
+return database;
+}
 //mysql --host=us-cdbr-iron-east-01.cleardb.net --user=b089b876f46b39 --password=e7e02c90 --reconnect heroku_cb42695d67403c5
-module.exports=database;
+module.exports=connect();
 
 
 exports.printf=function printf(str, params) {
